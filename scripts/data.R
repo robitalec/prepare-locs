d40a3e73f52a675c031d099eb8021f5015471bd9
Alec L. Robitaille <robit.alec@gmail.com>
2021-05-25 16:34:41 -0700

    add fogo

--- scripts/data.R
+++ scripts/data.R
@@ -1,11 +1,25 @@
-data.table(
-	path = ,
-	id = ,
-	datetime = ,
-	xcoord = ,
-	ycoord = ,
-	extracols ,
-	tz = ,
-	epsg =
-)
+# data.table(
+# 	path = ,
+# 	id = ,
+# 	datetime = ,
+# 	xcoord = ,
+# 	ycoord = ,
+# 	extracols ,
+# 	tz = ,
+# 	epsg =
+# )
+
+
+details <- rbindlist(list(
+	data.table(
+		path = '../fogo-caribou/data/FogoCaribou.csv',
+		id = 'ANIMAL_ID',
+		datetime = 'datetime',
+		xcoord = 'X_COORD',
+		ycoord = 'Y_COORD',
+		extracols = NA,
+		tz = grep('Newfoundland', OlsonNames(), value = TRUE),
+		epsg = 32621
+	)
+))
 
