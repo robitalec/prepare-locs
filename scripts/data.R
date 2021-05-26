4a38eec70fc6fe194af9a124f1db946f90aaa2d8
Alec L. Robitaille <robit.alec@gmail.com>
2021-05-25 17:15:41 -0700

    turn data details into function

--- scripts/data.R
+++ scripts/data.R
@@ -9,27 +9,29 @@
 # 	epsg =
 # )
 
+data_details <- function() {
+	rbindlist(list(
+		data.table(
+			path = '../fogo-caribou/data/FogoCaribou.csv',
+			id = 'ANIMAL_ID',
+			datetime = 'datetime',
+			xcoord = 'X_COORD',
+			ycoord = 'Y_COORD',
+			extracols = NA,
+			tz = grep('Newfoundland', OlsonNames(), value = TRUE),
+			epsg = 32621
+		),
+		data.table(
+			path = 'input/AllCaribouDataRaw.csv',
+			id = 'ANIMAL_ID',
+			datetime = 'datetime',
+			xcoord = 'X_COORD',
+			ycoord = 'Y_COORD',
+			extracols = list(list('HERD', 'Map_Quality', 'LOCQUAL')),
+			tz = grep('Newfoundland', OlsonNames(), value = TRUE),
+			epsg = 32621
+		)
+	))
+}
 
-details <- rbindlist(list(
-	data.table(
-		path = '../fogo-caribou/data/FogoCaribou.csv',
-		id = 'ANIMAL_ID',
-		datetime = 'datetime',
-		xcoord = 'X_COORD',
-		ycoord = 'Y_COORD',
-		extracols = NA,
-		tz = grep('Newfoundland', OlsonNames(), value = TRUE),
-		epsg = 32621
-	),
-	data.table(
-		path = 'input/AllCaribouDataRaw.csv',
-		id = 'ANIMAL_ID',
-		datetime = 'datetime',
-		xcoord = 'X_COORD',
-		ycoord = 'Y_COORD',
-		extracols = list(list('HERD', 'Map_Quality', 'LOCQUAL')),
-		tz = grep('Newfoundland', OlsonNames(), value = TRUE),
-		epsg = 32621
-	)
-))
 
