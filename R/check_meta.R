fbe4502273e57b3ffad2e3bd81265d2334af607c
Alec L. Robitaille <robit.alec@gmail.com>
2021-07-28 13:10:05 -0700

    fst check_meta

---
+++ R/check_meta.R
@@ -0,0 +1,10 @@
+#' Check metadata
+#' @param meta
+#' @return
+#' @author Alec L. Robitaille
+#' @export
+check_meta <- function(meta) {
+	N_name <- meta[, .N, by = name]$N
+
+	if (any(N_name) > 1) stop('found duplicated names, check metadata')
+}
