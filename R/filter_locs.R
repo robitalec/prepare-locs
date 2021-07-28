edd5083ecb8850bd7902b2e2c8b561eb657296c5
Alec L. Robitaille <robit.alec@gmail.com>
2021-07-28 14:30:38 -0700

    why flag why
    base::drop

--- R/filter_locs.R
+++ R/filter_locs.R
@@ -12,7 +12,7 @@
 filter_locs <- function(DT) {
 	check_longlat(DT)
 	check_locs_meta(DT)
-	DT[!is.na(drop), c('long', 'lat') := NaN]
+	DT[!is.na(flag), c('long', 'lat') := NaN]
 	return(DT)
 }
 
@@ -36,18 +36,18 @@ check_longlat <- function(DT) {
 	if (DT[, !is.numeric(long)]) DT[, long := is.numeric(long)]
 	if (DT[, !is.numeric(lat)]) DT[, lat := is.numeric(lat)]
 
-	DT[!between(long, -180, 360), drop := why(drop, 'long not between -180, 360')]
-	DT[!between(lat, -90, 90), drop := why(drop, 'lat not between -90, 90')]
+	DT[!between(long, -180, 360), flag := why(flag, 'long not between -180, 360')]
+	DT[!between(lat, -90, 90), flag := why(flag, 'lat not between -90, 90')]
 
-	DT[long == 0,  drop := why(drop, 'long is 0')]
-	DT[lat == 0,  drop := why(drop, 'lat is 0')]
+	DT[long == 0,  flag := why(flag, 'long is 0')]
+	DT[lat == 0,  flag := why(flag, 'lat is 0')]
 
-	DT[long == lat, drop := why(drop, 'long == lat')]
+	DT[long == lat, flag := why(flag, 'long == lat')]
 
-	DT[is.na(long), drop := why(drop, 'long is NA')]
-	DT[is.nan(long), drop := why(drop, 'long is NaN')]
-	DT[is.na(lat), drop := why(drop, 'lat is NA')]
-	DT[is.nan(lat), drop := why(drop, 'lat is NaN')]
+	DT[is.na(long), flag := why(flag, 'long is NA')]
+	DT[is.nan(long), flag := why(flag, 'long is NaN')]
+	DT[is.na(lat), flag := why(flag, 'lat is NA')]
+	DT[is.nan(lat), flag := why(flag, 'lat is NaN')]
 
 	DT
 }
@@ -73,12 +73,12 @@ check_locs_meta <- function(DT) {
 	# }
 
 	if ('Map_Quality' %in% colnames(DT)) {
-		DT[Map_Quality == 'N', drop := why(drop, 'Map_Quality is N')]
+		DT[Map_Quality == 'N', flag := why(flag, 'Map_Quality is N')]
 		DT[, Map_Quality := NULL]
 	}
 
 	if ('EXCLUDE' %in% colnames(DT)) {
-		DT[EXCLUDE == 'Y', drop := why(drop, 'EXCLUDE is Y')]
+		DT[EXCLUDE == 'Y', flag := why(flag, 'EXCLUDE is Y')]
 		DT[, EXCLUDE := NULL]
 	}
 
@@ -91,13 +91,13 @@ check_locs_meta <- function(DT) {
 	# TODO: VALIDATED
 
 	if ('COLLAR_TYPE_CL' %in% colnames(DT)) {
-		DT[COLLAR_TYPE_CL != 'GPS', drop := why(drop, paste0('Collar type is', COLLAR_TYPE_CL))]
+		DT[COLLAR_TYPE_CL != 'GPS', flag := why(flag, paste0('Collar type is', COLLAR_TYPE_CL))]
 		DT[, COLLAR_TYPE_CL := NULL]
 	}
 }
 
 
 
-why <- function(drop, why) {
-	data.table::fifelse(is.na(drop), why, paste(drop, why, sep = ', '))
+why <- function(flag, why) {
+	data.table::fifelse(is.na(flag), why, paste(flag, why, sep = ', '))
 }
