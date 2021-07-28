4a3f22ca5c6c7d7deef8425a581c2ebdd9507b92
Alec L. Robitaille <robit.alec@gmail.com>
2021-07-28 10:29:19 -0700

    mv to project_locs.R

---
+++ R/project_locs.R
@@ -0,0 +1,47 @@
+#' Project locs
+#' @inheritParams prep_dates
+#' @param EPSG numeric; EPSG code
+#'
+#' @return
+#' @export
+#'
+#' @examples
+project_locs <- function(DT, epsgin, epsgout) {
+	check_truelength(DT)
+	if (!is.numeric(epsgout)) stop("epsgout must be numeric")
+
+	coords <- c('long', 'lat')
+	projcoords <- paste0('proj', coords)
+
+	lapply(coords, function(x) check_col(DT, x))
+	lapply(coords, function(x) check_type(DT, x, 'double'))
+	lapply(projcoords, function(x) overwrite_col(DT, x))
+
+	if (is.na(as.numeric(epsgin))) {
+		if (!epsgin %in% colnames(DT)) {
+			stop('if epsgin is a character, it must refer to column name in DT')
+		}
+		check_type(DT, epsgin, 'integer')
+
+		DT[, (projcoords) :=
+			 	data.table::as.data.table(
+			 		sf::sf_project(
+			 			pts = as.matrix(.SD, ncol = 2),
+			 			from = sf::st_crs(.BY[[1]]),
+			 			keep = TRUE,
+			 			to = sf::st_crs(epsgout))
+			 	),
+			 .SDcols = coords,
+			 by = epsgin]
+	} else if (!is.na(as.numeric(epsgin))) {
+		DT[, (projcoords) :=
+			 	data.table::as.data.table(
+			 		sf::sf_project(
+			 			pts = as.matrix(.SD, ncol = 2),
+			 			from = sf::st_crs(as.numeric(epsgin)),
+			 			keep = TRUE,
+			 			to = sf::st_crs(epsgout))
+			 	),
+			 .SDcols = coords]
+	}
+}
