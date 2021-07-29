4c30ab8b5e6acc5b7349989273b8e10d6a906c2b
Alec L. Robitaille <robit.alec@gmail.com>
2021-07-29 15:50:44 -0700

    mv into if numeric

--- R/project_locs.R
+++ R/project_locs.R
@@ -14,23 +14,6 @@ project_locs <- function(DT, epsgin, epsgout) {
 	coords <- c('long', 'lat')
 	projcoords <- paste0('proj', coords)
 
-	if (!sf::st_is_longlat(st_crs(epsgin))) {
-		warning('epsgin is not long lat, checks will break')
-		# TODO: make this error after fix RMNP
-	}
-
-	if (sf::st_is_longlat(st_crs(epsgout))) {
-		stop('epsgout must not be long lat, unprojected')
-	}
-
-	if (epsgin == epsgout) {
-		warning('epsgin is equal to epsgout')
-		warning('are you sure your input coordinates are already projected?')
-
-		DT[, (projcoords) := .SD, .SDcols = c(coords)]
-		return(DT)
-	}
-
 	lapply(coords, function(x) check_col(DT, x))
 	lapply(coords, function(x) check_type(DT, x, 'double'))
 	lapply(projcoords, function(x) overwrite_col(DT, x))
@@ -54,6 +37,23 @@ project_locs <- function(DT, epsgin, epsgout) {
 		# data.table::set(DT, j = epsgin, value = NULL)
 		# DT
 	} else if (!is.na(as.numeric(epsgin))) {
+		if (!sf::st_is_longlat(st_crs(epsgin))) {
+			warning('epsgin is not long lat, checks will break')
+			# TODO: make this error after fix RMNP
+		}
+
+		if (sf::st_is_longlat(st_crs(epsgout))) {
+			stop('epsgout must not be long lat, unprojected')
+		}
+
+		if (epsgin == epsgout) {
+			warning('epsgin is equal to epsgout')
+			warning('are you sure your input coordinates are already projected?')
+
+			DT[, (projcoords) := .SD, .SDcols = c(coords)]
+			return(DT)
+		}
+
 		DT[, (projcoords) :=
 			 	data.table::as.data.table(
 			 		sf::sf_project(
