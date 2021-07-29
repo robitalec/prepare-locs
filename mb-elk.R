0eae2424617e611a67636f35bd756ff8c582e429
Alec L. Robitaille <robit.alec@gmail.com>
2021-07-29 14:04:52 -0700

    use raw csvs

--- mb-elk.R
+++ mb-elk.R
@@ -1,23 +1,11 @@
 library(sf)
 library(data.table)
 
-vita_lotek <- readRDS('input/elk-data/vita_elk_lotek_feb_2016-july_2019_cleaned.rds')
-
-DT <- st_drop_geometry(vita_lotek)
-setDT(DT)
-
-fwrite(DT, 'input/vita_elk_lotek.csv')
-
-vita_vec <- readRDS('input/elk-data/vita_elk_vectronic_feb_2019-march_2021_cleaned.rds')
-
-DT <- st_drop_geometry(vita_vec)
-setDT(DT)
-
-fwrite(DT, 'input/vita_elk_vectronic.csv')
+vita_lotek <- fread('input/vita_elk_lotek_feb_2016-july_2019.csv')
+vita_vec <- fread('input/vita_elk_vectronic_feb_2019-march_2021.csv')
 
 
 # why separated lotek and vectronic
 # EPSG out?
-
-rmnp <- fread('input/elk-data/RMNPdata2006-2015CleanedLN.csv')
-fwrite(rmnp, 'input/rmnp_elk.csv')
+rmnp <- fread('input/RMNPdata2006-2015CleanedLN.csv')
+fwrite(rmnp, 'input/rmnp_elk_2006_2015.csv')
