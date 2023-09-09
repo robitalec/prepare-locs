e581d84a79b838b65ab1eb90f20863feba8ca538
Alec L. Robitaille <robit.alec@gmail.com>
2023-09-09 12:31:33 -0300

    fst prep_deployment function

---
+++ R/prep_deployment.R
@@ -0,0 +1,32 @@
+#' Prep deployment sheet where necessary
+#'
+#' Currently only Fogo
+#'
+#' @param meta
+#'
+#' @return
+#' meta updated with new path to prepared deployment sheet
+#' @export
+#' @author Alec L. Robitaille
+#'
+#' @examples
+prep_deployment <- function(meta) {
+	if (!is.na(meta$deployment) &&
+			grepl('NL-Fogo-Caribou-Telemetry', meta$name) &&
+			file.exists(meta$deployment)) {
+
+		deploy <- fread(meta$deployment)
+
+		prepared <- deploy[, .(start_date = parse_date(`Capture date`),
+													 end_date = parse_date(`Mortality Date`)),
+											 by = .(animal_id = `Animal No.`, collar_id = ID)]
+
+		prepared_path <- file.path('input', paste0(meta$name, '_deployment.csv'))
+		fwrite(prepared, prepared_path)
+
+		meta$deployment <- prepared_path
+		meta
+	} else {
+		meta
+	}
+}
