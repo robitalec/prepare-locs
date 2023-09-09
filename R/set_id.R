4646d672dc44f3c381c0a13533e05f3bbf1895e8
Alec L. Robitaille <robit.alec@gmail.com>
2023-09-09 11:59:19 -0300

    fst set id function

---
+++ R/set_id.R
@@ -0,0 +1,8 @@
+#' Set id from collar id and deployment
+#'
+#' @param DT
+#' @param name
+#' @return
+#' Input DT with id appended from matching collar id / deployment sheet
+#' @export
+#' @author Alec L. Robitaille
