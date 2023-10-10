02bcdbe5979f8a6515b207d72c4cbef6811c48a8
Alec L. Robitaille <robit.alec@gmail.com>
2023-10-10 16:15:55 -0300

    read deploy if exists, set end to today

--- R/read_deployment.R
+++ R/read_deployment.R
@@ -9,20 +9,10 @@
 #'
 #' @examples
 read_deployment <- function(deployment) {
-	if (!is.na(deployment) &&
-			grepl('NL-Fogo-Caribou-Telemetry', meta$name) &&
-			file.exists(deployment)) {
-
+	if (!is.na(deployment) && file.exists(deployment)) {
 		deploy <- fread(deployment)
 
 		deploy[is.na(end_date), end_date := Sys.Date()]
-
-		prepared_path <- file.path('input', paste0(meta$name, '_deployment.csv'))
-		fwrite(prepared, prepared_path)
-
-		meta$deployment <- prepared_path
-		meta
-	} else {
-		meta
+		return(deploy)
 	}
 }
