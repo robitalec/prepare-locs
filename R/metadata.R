9b2b29db0e83fa82bfea3466ab4ee6a61bb4f19f
Alec L. Robitaille <robit.alec@gmail.com>
2024-01-25 17:45:58 -0400

    fix 6030

--- R/metadata.R
+++ R/metadata.R
@@ -68,7 +68,7 @@ metadata <- function() {
 			time = 'FIX_TIME',
 			extracols = list(list('COLLAR_TYPE_CL', 'HERD', 'Map_Quality', 'EPSG_CODE', 'EXCLUDE', 'DOP', 'NAV')),
 			tz = 'UTC',
-			epsgin = 'EPSG_CODE',
+			epsgin = 4326,
 			epsgout = 32621,
 			splitBy = 'HERD'
 		),
@@ -84,7 +84,7 @@ metadata <- function() {
 			time = 'FIX_TIME',
 			extracols = list(list('COLLAR_TYPE_CL', 'HERD', 'Map_Quality', 'EPSG_CODE', 'EXCLUDE', 'DOP', 'NAV')),
 			tz = 'UTC',
-			epsgin = 'EPSG_CODE',
+			epsgin = 4326,
 			epsgout = 32621,
 			splitBy = 'HERD'
 		),
@@ -100,7 +100,7 @@ metadata <- function() {
 			time = 'FIX_TIME',
 			extracols = list(list('COLLAR_TYPE_CL', 'HERD', 'Map_Quality', 'EPSG_CODE', 'EXCLUDE', 'DOP', 'NAV')),
 			tz = 'UTC',
-			epsgin = 'EPSG_CODE',
+			epsgin = 4326,
 			epsgout = 32621,
 			splitBy = 'HERD'
 		),
