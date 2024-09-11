30837c2e361df1838718247deefa52ef17ead2db
Alec L. Robitaille <robit.alec@gmail.com>
2024-09-11 14:26:35 -0300

    set id

--- R/read_data.R
+++ R/read_data.R
@@ -30,6 +30,9 @@ read_data <- function(path, meta, deploy) {
 				'DOP', 'Device ID'),
 			c('V10', 'V9', 'V2', 'V3', 'V13', 'V12', 'collar_id')
 		)
+
+		set_id(DT_lotek, meta$name, deploy)
+
 		# Without headers
 		regex_with_headers <- 'old_collars|Collar00993_FO2016005|Collar01082_FO2016002'
 		without_headers <- files[grep(regex_with_headers, files, invert = TRUE)]
